0438dafcdbc137294c92fd3f87af5afadde41024
Christian Krause <kizkizzbangbang@googlemail.com>
2016-04-26 19:17:51 +0200

    adds runFoldMap as stream op
    - addresses #1
    - implemented in terms of `Stream.runFold`

--- src/main/scala/fs2/interop/cats/cats.scala
+++ src/main/scala/fs2/interop/cats/cats.scala
@@ -4,6 +4,8 @@ package interop
 import _root_.cats.Monoid
 import _root_.cats.Semigroup
 
+import fs2.util.Free
+
 package object cats extends Instances {
 
   object reverse extends ReverseInstances
@@ -11,7 +13,7 @@ package object cats extends Instances {
   implicit class StreamCatsOps[F[_], A](val self: Stream[F, A]) extends AnyVal {
 
     def foldMap[B](f: A => B)(implicit M: Monoid[B]): Stream[F, B] =
-      self.fold(M.empty)((b,a) => M.combine(b, f(a)))
+      self.fold(M.empty)((b, a) => M.combine(b, f(a)))
 
     def foldMonoid(implicit M: Monoid[A]): Stream[F, A] =
       self.fold(M.empty)(M.combine(_, _))
@@ -19,6 +21,10 @@ package object cats extends Instances {
     def foldSemigroup(implicit S: Semigroup[A]): Stream[F, A] =
       self.reduce(S.combine(_, _))
 
+    def runFoldMap[B](f: A => B)(implicit M: Monoid[B]): Free[F, B] =
+      self.runFold(M.empty)((b, a) => M.combine(b, f(a)))
+
   }
 
 }
+
