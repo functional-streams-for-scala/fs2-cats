4758e7a4f11cc8a6864ba2ce5493b491cb106348
Christian Krause <kizkizzbangbang@googlemail.com>
2016-04-27 13:12:35 +0200

    adds runGroupBy* functions
    - all implemented in terms of runFoldMap

--- src/main/scala/fs2/interop/cats/cats.scala
+++ src/main/scala/fs2/interop/cats/cats.scala
@@ -1,8 +1,11 @@
 package fs2
 package interop
 
+import _root_.cats.Applicative
 import _root_.cats.Monoid
 import _root_.cats.Semigroup
+
+import _root_.cats.std.map.mapMonoid
 
 import fs2.util.Free
 
@@ -23,6 +26,15 @@ package object cats extends Instances {
 
     def runFoldMap[B](f: A => B)(implicit M: Monoid[B]): Free[F, B] =
       self.runFold(M.empty)((b, a) => M.combine(b, f(a)))
+
+    def runGroupByFoldMap[K, B: Monoid](f: A => K)(g: A => B): Free[F, Map[K, B]] =
+      runFoldMap(a => Map(f(a) -> g(a)))
+
+    def runGroupByFoldMonoid[K](f: A => K)(implicit M: Monoid[A]): Free[F, Map[K, A]] =
+      runFoldMap(a => Map(f(a) -> a))
+
+    def runGroupBy[X[_], K](f: A => K)(implicit A: Applicative[X], M: Monoid[X[A]]): Free[F, Map[K, X[A]]] =
+      runGroupByFoldMap(f)(A.pure)
 
   }
 
