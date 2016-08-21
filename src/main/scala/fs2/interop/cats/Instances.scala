476d0cb1dcfc5e3233123a3e26fb86b3674cd330
Ross A. Baker <ross@rossabaker.com>
2016-08-21 14:13:52 -0400

    Upgrade to cats-0.7.0

--- src/main/scala/fs2/interop/cats/Instances.scala
+++ src/main/scala/fs2/interop/cats/Instances.scala
@@ -1,20 +1,20 @@
 package fs2.interop.cats
 
 import fs2.util._
-import _root_.cats.{ Eval, Functor => CatsFunctor, Monad => CatsMonad, MonadError }
-import _root_.cats.arrow.NaturalTransformation
+import _root_.cats.{ Functor => CatsFunctor, Monad => CatsMonad, MonadError }
+import _root_.cats.arrow.FunctionK
 
 trait Instances extends Instances0 {
   implicit def effectToMonadError[F[_]](implicit F: Effect[F]): MonadError[F, Throwable] = new MonadError[F, Throwable] {
     def pure[A](a: A) = F.pure(a)
-    override def pureEval[A](a: Eval[A]) = F.delay(a.value)
     override def map[A, B](fa: F[A])(f: A => B) = F.map(fa)(f)
     def flatMap[A, B](fa: F[A])(f: A => F[B]) = F.flatMap(fa)(f)
+    def tailRecM[A, B](a: A)(f: A => F[Either[A,B]]): F[B] = defaultTailRecM(a)(f)
     def raiseError[A](t: Throwable) = F.fail(t)
     def handleErrorWith[A](fa: F[A])(f: Throwable => F[A]) = F.flatMap(F.attempt(fa))(e => e.fold(f, pure))
   }
 
-  implicit def uf1ToNaturalTransformation[F[_], G[_]](implicit uf1: UF1[F, G]): NaturalTransformation[F, G] = new NaturalTransformation[F, G] {
+  implicit def uf1ToFunctionK[F[_], G[_]](implicit uf1: UF1[F, G]): FunctionK[F, G] = new FunctionK[F, G] {
     def apply[A](fa: F[A]) = uf1(fa)
   }
 }
@@ -24,6 +24,7 @@ private[cats] trait Instances0 extends Instances1 {
     def pure[A](a: A) = F.pure(a)
     override def map[A, B](fa: F[A])(f: A => B) = F.map(fa)(f)
     def flatMap[A, B](fa: F[A])(f: A => F[B]) = F.flatMap(fa)(f)
+    def tailRecM[A, B](a: A)(f: A => F[Either[A,B]]): F[B] = defaultTailRecM(a)(f)
     def raiseError[A](t: Throwable) = F.fail(t)
     def handleErrorWith[A](fa: F[A])(f: Throwable => F[A]) = F.flatMap(F.attempt(fa))(e => e.fold(f, pure))
   }
@@ -34,6 +35,7 @@ private[cats] trait Instances1 extends Instances2 {
     def pure[A](a: A) = F.pure(a)
     override def map[A, B](fa: F[A])(f: A => B) = F.map(fa)(f)
     def flatMap[A, B](fa: F[A])(f: A => F[B]) = F.flatMap(fa)(f)
+    def tailRecM[A, B](a: A)(f: A => F[Either[A,B]]): F[B] = defaultTailRecM(a)(f)
   }
 }
 
